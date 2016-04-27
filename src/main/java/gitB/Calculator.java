f4016373bf86b0142aac2997f14f0ce5f6b8251f
1192002고준호 <junoko91@naver.com>
2016-04-27 16:48:07 +0900

    err

--- src/main/java/gitB/Calculator.java
+++ src/main/java/gitB/Calculator.java
@@ -4,10 +4,10 @@ public class Calculator {
 	private int result=0;
 	
 	public void add(int x, int y) {
-		result=x-y;
+		result=x+y;
 	}
 	public void sub(int x,int y){
-		result=x+y;
+		result=x-y;
 	}
 	public int getResult()  {
 		return result;
